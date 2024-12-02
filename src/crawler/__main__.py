70dfed0565667a3326af357a1348bb383d08fb8e
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2024-12-02 10:58:23 +0800

    doc

--- src/crawler/__main__.py
+++ src/crawler/__main__.py
@@ -5,7 +5,7 @@ Copyright (c) 2023-present 善假于PC也 (zlhywlf).
 
 
 def main() -> None:
-    """The powerful-crawler application."""
+    """The powerful crawler application."""
 
 
 if __name__ == "__main__":
