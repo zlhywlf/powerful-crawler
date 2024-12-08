2841a43fb60f5adb867ee59e5f225c9b210a930e
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2024-12-08 22:05:28 +0800

    save to fs

--- src/crawler/processors/MasterProcessor.py
+++ src/crawler/processors/MasterProcessor.py
@@ -3,6 +3,8 @@
 Copyright (c) 2023-present 善假于PC也 (zlhywlf).
 """
 
+import asyncio
+
 import scrapy
 
 from crawler.models.Result import Result
@@ -14,4 +16,7 @@ class MasterProcessor:
     async def __call__(self, item: Result, spider: scrapy.Spider) -> Result:
         """Do."""
         spider.log(f"{item.id},{item.type},{len(item.content)}")
+        loop = asyncio.get_event_loop()
+        with open(f"{item.id}_{len(item.content)}.{'html' if 'html' in item.type else 'unknown'}", "wb") as f:  # noqa: ASYNC230 PTH123
+            await loop.run_in_executor(None, f.write, item.content)
         return item
