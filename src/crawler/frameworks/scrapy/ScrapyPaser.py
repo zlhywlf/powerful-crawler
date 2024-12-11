d237a733db76b7ae57e43626c0da8a213731278c
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2024-12-11 14:04:37 +0800

    optimize

--- src/crawler/frameworks/scrapy/ScrapyPaser.py
+++ src/crawler/frameworks/scrapy/ScrapyPaser.py
@@ -9,11 +9,11 @@ from typing import Any
 from scrapy.http.response import Response
 
 from crawler.core.Request import Request
-from crawler.decisions.pasers.ListPageDecisionNode import ListPageDecisionNode
-from crawler.decisions.pasers.NextPageDecisionNode import NextPageDecisionNode
-from crawler.decisions.pasers.PagingDecisionNode import PagingDecisionNode
+from crawler.decisions.pasers.nodes.ListPageDecisionNode import ListPageDecisionNode
+from crawler.decisions.pasers.nodes.NextPageDecisionNode import NextPageDecisionNode
+from crawler.decisions.pasers.nodes.PagingDecisionNode import PagingDecisionNode
+from crawler.decisions.pasers.nodes.SavePageDecisionNode import SavePageDecisionNode
 from crawler.decisions.pasers.ParserDecisionEngine import ParserDecisionEngine
-from crawler.decisions.pasers.SavePageDecisionNode import SavePageDecisionNode
 from crawler.frameworks.scrapy.ScrapyRequestFactory import ScrapyRequestFactory
 from crawler.frameworks.scrapy.ScrapyResponse import ScrapyResponse
 from crawler.models.dto.Context import Context
